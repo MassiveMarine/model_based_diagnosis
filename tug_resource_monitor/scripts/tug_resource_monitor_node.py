c7bd9ef7964937550b4d0e978aded6ec33ae1ea2
Stefan Loigge <stefan.loigge@student.tugraz.at>
2015-07-17 15:57:46 +0200

    created python class

--- tug_resource_monitor/scripts/tug_resource_monitor_node.py
+++ tug_resource_monitor/scripts/tug_resource_monitor_node.py
@@ -7,79 +7,93 @@ import rosnode
 from tug_resource_monitor.msg import NodeInfo, NodeInfoArray
 import psutil
 
-processes = dict()
 
+class TUGResourceMonitor:
+    def __init__(self):
+        self.processes = dict()  # processes are NEVER removed from this dict
+        self.master = rosnode.rosgraph.Master(rosnode.ID)
+        node_api = rosnode.get_api_uri(self.master, rospy.get_name())
+        self.own_hostname = rosnode.urlparse.urlparse(node_api).hostname
 
-def get_cpu_and_mem_usage(pids=[]):
-    process_info = dict()
+        self.node_infos_pub = rospy.Publisher('diag/node_infos', NodeInfoArray, queue_size=10)
 
-    for pid in pids:
-        try:
-            if not processes.has_key(pid):
-                processes[pid] = psutil.Process(pid)
+    def get_cpu_and_mem_usage(self, pids):
+        process_info = dict()
 
-            memory_info = processes[pid].get_memory_info()[0]
-            cpu_percent = processes[pid].get_cpu_percent(interval=0)
-
-            process_info[pid] = [cpu_percent, memory_info]
-        except psutil.NoSuchProcess, KeyError:
-            pass
-
-    return process_info
-
-
-def get_node_names_and_pids():
-    process_info = dict()
-
-    node_names = rosnode.get_node_names()
-    master = rosnode.rosgraph.Master(rosnode.ID)
-
-    for node in node_names:
-        node_api = rosnode.get_api_uri(master, node)
-        hostname = rosnode.urlparse.urlparse(node_api).hostname
-
-        if not node_api:
-            print("cannot find [%s]: unknown node" % node)
-            continue
-        try:
-            code, msg, pid = rosnode.ServerProxy(node_api).getPid(rosnode.ID)
-            if code != 1:
-                raise rosnode.ROSNodeException("remote call failed: %s" % msg)
-        except:
-            continue
-
-        process_info[pid] = [node, hostname]
-    return process_info
-
-
-def run(frequency=1.0):
-
-    rate = rospy.Rate(frequency)
-    while not rospy.is_shutdown():
-
-        nodes_info_array = NodeInfoArray()
-
-        nodes_info = get_node_names_and_pids()
-        process_info = get_cpu_and_mem_usage(nodes_info.keys())
-
-        for pid, [node, hostname] in nodes_info.iteritems():
+        for pid in pids:
             try:
-                nodes_info_array.data.append(NodeInfo(name=node, pid=pid, hostname=hostname,
-                                                      cpu=process_info[pid][0], memory=process_info[pid][1]))
-            except KeyError:
-                rospy.logerr("error with PID of node '" + node + "' on machine '" + hostname + "'")
+                if not self.processes.has_key(pid):
+                    self.processes[pid] = psutil.Process(pid)
 
-        node_infos_pub.publish(nodes_info_array)
-        rate.sleep()
+                memory_info = self.processes[pid].get_memory_info()[0]
+                cpu_percent = self.processes[pid].get_cpu_percent(interval=0)
+
+                process_info[pid] = [cpu_percent, memory_info]
+            except psutil.NoSuchProcess:
+                pass
+
+        return process_info
+
+    def get_node_names_and_pids(self):
+        process_info = dict()
+
+        node_names = rosnode.get_node_names()
+        master = rosnode.rosgraph.Master(rosnode.ID)
+
+        for node in node_names:
+            try:
+                node_api = rosnode.get_api_uri(master, node)
+                hostname = rosnode.urlparse.urlparse(node_api).hostname
+
+                # check if node can be found
+                if not node_api:
+                    rospy.logwarn("cannot find '" + node + "': unknown node")
+                    continue
+
+                # only take nodes, which are running on this machine
+                if hostname != self.own_hostname:
+                    continue
+
+                # read out pid of node
+                code, msg, pid = rosnode.ServerProxy(node_api).getPid(rosnode.ID)
+                if code != 1:
+                    rospy.logwarn("remote call failed: '" + node + "'")
+
+                process_info[pid] = [node, hostname]
+
+            except rosnode.ROSNodeIOException as error:
+                rospy.logerr(error)
+            except IOError as error:
+                rospy.logerr(error)
+
+        return process_info
+
+    def run(self, frequency=1.0):
+
+        rate = rospy.Rate(frequency)
+        while not rospy.is_shutdown():
+
+            nodes_info_array = NodeInfoArray()
+
+            nodes_info = self.get_node_names_and_pids()
+            process_info = self.get_cpu_and_mem_usage(nodes_info.keys())
+
+            for pid, [node, hostname] in nodes_info.iteritems():
+                try:
+                    nodes_info_array.data.append(NodeInfo(name=node, pid=pid, hostname=hostname,
+                                                          cpu=process_info[pid][0], memory=process_info[pid][1]))
+                except KeyError:
+                    rospy.logerr("error with PID of node '" + node + "'")
+
+            self.node_infos_pub.publish(nodes_info_array)
+            rate.sleep()
 
 if __name__ == "__main__":
-    rospy.init_node('tug_resource_monitor', anonymous=True)
-
-    node_infos_pub = rospy.Publisher('diag/node_infos', NodeInfoArray, queue_size=10)
+    rospy.init_node('tug_resource_monitor', anonymous=False)
 
     try:
         rospy.loginfo("starting " + rospy.get_name())
-        run(1)
+        TUGResourceMonitor().run(1)
     except KeyboardInterrupt:
         pass
     except rosnode.ROSNodeIOException as e:
