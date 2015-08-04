f8e6a190f46111b7fd43c3b9ad505905039e630e
Clemens Muehlbacher <cmuehlba@ist.tugraz.at>
2015-08-04 09:46:24 +0200

    first compiling version of cpp observers

--- tug_observers/tug_observers_cpp/include/tug_observers_cpp/ObserverPluginBase.h
+++ tug_observers/tug_observers_cpp/include/tug_observers_cpp/ObserverPluginBase.h
@@ -8,6 +8,10 @@
 #include <boost/thread.hpp>
 #include <ros/callback_queue.h>
 #include <ros/spinner.h>
+#include <ros/subscriber.h>
+#include <ros/subscribe_options.h>
+#include <ros/transport_hints.h>
+#include <ros/node_handle.h>
 
 namespace tug_observers_cpp
 {
@@ -17,30 +21,55 @@ namespace tug_observers_cpp
     private:
       ros::CallbackQueue internal_call_back_queue_;
       ros::AsyncSpinner spinner_;
+      ros::NodeHandle nh_;
 
       /// helper functions in order to clean up subscriber functions
-      template<class M, class T>
-      Subscriber subscribe(const std::string &topic, uint32_t queue_size, const boost::function<void(P)> &callback,
-                           const TransportHints &transport_hints,
-                           const boost::shared_ptr<T> &obj = boost::share_ptr<T>()
+      template<class P>
+      ros::Subscriber subscribe(const std::string &topic, uint32_t queue_size, const boost::function<void(P)> &callback,
+                           const ros::TransportHints &transport_hints
       )
       {
-        SubscribeOptions ops;
-        ops.template initByFullCallbackType<M>(topic, queue_size, callback);
+        ros::SubscribeOptions ops;
+        ops.template initByFullCallbackType<P>(topic, queue_size, callback);
+        ops.transport_hints = transport_hints;
+        ops.callback_queue = internal_call_back_queue_;
+        return nh_.subscribe(ops);
+      }
+
+      template<class P>
+      ros::Subscriber subscribeConst(const std::string &topic, uint32_t queue_size, const boost::function<void(P)> &callback,
+                                const ros::TransportHints &transport_hints
+      )
+      {
+        ros::SubscribeOptions ops;
+        ops.template init<P>(topic, queue_size, callback);
+        ops.transport_hints = transport_hints;
+        ops.callback_queue = internal_call_back_queue_;
+        return nh_.subscribe(ops);
+      }
+
+      template<class P, class T>
+      ros::Subscriber subscribe(const std::string &topic, uint32_t queue_size, const boost::function<void(P)> &callback,
+                                const ros::TransportHints &transport_hints,
+                                const boost::shared_ptr<T> &obj
+      )
+      {
+        ros::SubscribeOptions ops;
+        ops.template initByFullCallbackType<P>(topic, queue_size, callback);
         ops.transport_hints = transport_hints;
         ops.tracked_object = obj;
         ops.callback_queue = internal_call_back_queue_;
         return nh_.subscribe(ops);
       }
 
-      template<class M, class T>
-      Subscriber subscribeConst(const std::string &topic, uint32_t queue_size, const boost::function<void(P)> &callback,
-                                const TransportHints &transport_hints,
-                                const boost::shared_ptr<T> &obj = boost::share_ptr<T>()
+      template<class P, class T>
+      ros::Subscriber subscribeConst(const std::string &topic, uint32_t queue_size, const boost::function<void(P)> &callback,
+                                     const ros::TransportHints &transport_hints,
+                                     const boost::shared_ptr<T> &obj
       )
       {
-        SubscribeOptions ops;
-        ops.template init<M>(topic, queue_size, callback);
+        ros::SubscribeOptions ops;
+        ops.template init<P>(topic, queue_size, callback);
         ops.transport_hints = transport_hints;
         ops.tracked_object = obj;
         ops.callback_queue = internal_call_back_queue_;
@@ -59,8 +88,8 @@ namespace tug_observers_cpp
 
       /// subscriber functions in order to use the callback queue in the right way
       template<class M, class T>
-      Subscriber subscribe(const std::string &topic, uint32_t queue_size, void(T::*fp)(M), T *obj,
-                           const TransportHints &transport_hints = TransportHints()
+      ros::Subscriber subscribe(const std::string &topic, uint32_t queue_size, void(T::*fp)(M), T *obj,
+                           const ros::TransportHints &transport_hints = ros::TransportHints()
       )
       {
         return subscribe(topic, queue_size, boost::bind(fp, obj, _1), transport_hints);
@@ -68,57 +97,57 @@ namespace tug_observers_cpp
 
       /// and the const version
       template<class M, class T>
-      Subscriber subscribe(const std::string &topic, uint32_t queue_size, void(T::*fp)(M) const, T *obj,
-                           const TransportHints &transport_hints = TransportHints()
+      ros::Subscriber subscribe(const std::string &topic, uint32_t queue_size, void(T::*fp)(M) const, T *obj,
+                           const ros::TransportHints &transport_hints = ros::TransportHints()
       )
       {
         return subscribe(topic, queue_size, boost::bind(fp, obj, _1), transport_hints);
       }
 
       template<class M, class T>
-      Subscriber subscribe(const std::string &topic, uint32_t queue_size,
+      ros::Subscriber subscribe(const std::string &topic, uint32_t queue_size,
                            void(T::*fp)(const boost::shared_ptr<M const> &), T *obj,
-                           const TransportHints &transport_hints = TransportHints()
+                           const ros::TransportHints &transport_hints = ros::TransportHints()
       )
       {
         return subscribeConst(topic, queue_size, boost::bind(fp, obj, _1), transport_hints);
       }
 
       template<class M, class T>
-      Subscriber subscribe(const std::string &topic, uint32_t queue_size,
+      ros::Subscriber subscribe(const std::string &topic, uint32_t queue_size,
                            void(T::*fp)(const boost::shared_ptr<M const> &) const, T *obj,
-                           const TransportHints &transport_hints = TransportHints()
+                           const ros::TransportHints &transport_hints = ros::TransportHints()
       )
       {
         return subscribeConst(topic, queue_size, boost::bind(fp, obj, _1), transport_hints);
       }
 
       template<class M, class T>
-      Subscriber subscribe(const std::string &topic, uint32_t queue_size, void(T::*fp)(M),
-                           const boost::shared_ptr<T> &obj, const TransportHints &transport_hints = TransportHints())
+      ros::Subscriber subscribe(const std::string &topic, uint32_t queue_size, void(T::*fp)(M),
+                           const boost::shared_ptr<T> &obj, const ros::TransportHints &transport_hints = ros::TransportHints())
       {
         return subscribe(topic, queue_size, boost::bind(fp, obj.get(), _1), transport_hints, obj);
       }
 
       template<class M, class T>
-      Subscriber subscribe(const std::string &topic, uint32_t queue_size, void(T::*fp)(M) const,
-                           const boost::shared_ptr<T> &obj, const TransportHints &transport_hints = TransportHints())
+      ros::Subscriber subscribe(const std::string &topic, uint32_t queue_size, void(T::*fp)(M) const,
+                           const boost::shared_ptr<T> &obj, const ros::TransportHints &transport_hints = ros::TransportHints())
       {
         return subscribe(topic, queue_size, boost::bind(fp, obj.get(), _1), transport_hints, obj);;
       }
 
       template<class M, class T>
-      Subscriber subscribe(const std::string &topic, uint32_t queue_size,
+      ros::Subscriber subscribe(const std::string &topic, uint32_t queue_size,
                            void(T::*fp)(const boost::shared_ptr<M const> &),
-                           const boost::shared_ptr<T> &obj, const TransportHints &transport_hints = TransportHints())
+                           const boost::shared_ptr<T> &obj, const ros::TransportHints &transport_hints = ros::TransportHints())
       {
         return subscribeConst(topic, queue_size, boost::bind(fp, obj.get(), _1), transport_hints, obj);
       }
 
       template<class M, class T>
-      Subscriber subscribe(const std::string &topic, uint32_t queue_size,
+      ros::Subscriber subscribe(const std::string &topic, uint32_t queue_size,
                            void(T::*fp)(const boost::shared_ptr<M const> &) const,
-                           const boost::shared_ptr<T> &obj, const TransportHints &transport_hints = TransportHints())
+                           const boost::shared_ptr<T> &obj, const ros::TransportHints &transport_hints = ros::TransportHints())
       {
         return subscribeConst(topic, queue_size, boost::bind(fp, obj.get(), _1), transport_hints, obj);
       }
--- tug_observers/tug_observers_cpp/src/ObserverNode.cpp
+++ tug_observers/tug_observers_cpp/src/ObserverNode.cpp
@@ -13,10 +13,56 @@ namespace tug_observers_cpp
     {
       XmlRpc::XmlRpcValue params;
       nh_.getParam("", params);
+
+      if (!params.valid())
+      {
+        ROS_ERROR("No Plugins given");
+        throw std::runtime_error("No Plugins given");
+      }
+
+      for (XmlRpc::XmlRpcValue::iterator it = params.begin(); it != params.end(); ++it)
+      {
+        XmlRpc::XmlRpcValue & param = it->second;
+
+        if (!param.hasMember("type"))
+          throw std::runtime_error("/" + it->first + " has no 'type' parameter");
+
+        ObserverPluginBasePtr new_plugin = plugin_manager_.loadPlugin(it->first, param["type"]);
+        new_plugin->initialize(ros::NodeHandle(nh_, it->first));
+      }
     }
 
     void ObserverNode::startPlugins()
     {
-
+      Observers observers = plugin_manager_.getPluginList();
+      for(Observers::iterator it = observers.begin(); it != observers.end(); ++it)
+      {
+        it->instance->startPlugin();
+      }
     }
+}
+
+int main(int argc, char** argv)
+{
+  try
+  {
+    ros::init(argc, argv, "tug_robot_control_node");
+    ros::NodeHandle nh;
+    tug_observers_cpp::ObserverNode node(nh);
+    node.initPlugins();
+    node.startPlugins();
+  }
+  catch (std::exception & ex)
+  {
+    std::cerr << "Uncaught exception in main: " << ex.what() << std::endl;
+    return -1;
+  }
+  catch (...)
+  {
+    std::cerr << "Uncaught exception in main" << std::endl;
+    throw;
+    return -1;
+
+  }
+  return -1;
 }
