64f6b4f8bb7a7c5c0bf18322634b10b75a1ff513
Stefan Loigge <stefan.loigge@student.tugraz.at>
2015-09-03 17:29:28 +0200

    Fundamental changes of handling callerids of a topic. Each callerid is added to a list. Now it is possible to merge callerids for hypothesis check.

--- tug_observers/tug_observer_plugins_python/scripts/hz.py
+++ tug_observers/tug_observer_plugins_python/scripts/hz.py
@@ -43,58 +43,49 @@ class HzState():
 
 
 class HzBase():
-    def __init__(self, topic, config=None):
-        if not config:
-            self.cb = self.cb_empty
-            self.is_dummy_base = True
-            return
-
-        self.is_dummy_base = False
-
+    def __init__(self, topic, callerid, config):
+        # create a filter and lock
+        self._filter_lock = Lock()
         self._filter = Filter(Config.get_param(config, 'filter'))
 
-        self._states = []
-        for state_config in Config.get_param(config, 'states'):
-            try:
-                self._states.append(HzState(state_config))
-            except KeyError as e:
-                rospy.logerr(e)
-
-
-        self.topic = topic
-        self.callerid = Config.get_param(config, 'callerid')
-        self._resource_info = resource_info(type='hz', resource=str(self.topic + ' ' + self.callerid))
-        self._observer_error = observer_error(type='hz', resource=str(self.topic + ' ' + self.callerid))
-
-        self._lock = Lock()
-        timeout = Config.get_param(config, 'timeout')
-        self._event = PluginTimeout(timeout, self.timeout_cb)
-
+        # necessary stuff to calculate time between cb's
         self._msg_t0 = -1.
         self._msg_tn = 0
 
+        # requirements to handle timeout
+        self._event = PluginTimeout(Config.get_param(config, 'timeout'), self.timeout_cb)
+        self.max_timeouts = 10 if len(Config.get_param(config, 'callerid')) < 1 else None
+        self.remaining_timeouts = self.max_timeouts
+
+        # create a predefined error msg
+        self._observer_error = observer_error(type='hz', resource=str(topic + ' ' + str(callerid)))
+
     def timeout_cb(self):
-        # rospy.logwarn('timeout of topic')
+        # publish error
         if error_pub:
             self._observer_error.header = rospy.Header(stamp=rospy.Time.now())
             self._observer_error.error_msg = resource_error_timeout
             error_pub.publish(self._observer_error)
-        self._lock.acquire()
+
+        self._filter_lock.acquire()
+        # reset all necessary things to be ready for restart
         self._msg_t0 = -1.
         self._msg_tn = 0
         self._filter.reset()
-        self._lock.release()
 
-    def cb_empty(self, msg):
-        print 'empty cb'
-        pass
+        # decrement timeout counter
+        if self.max_timeouts is not None:
+            self.remaining_timeouts -= 1
+
+        self._filter_lock.release()
 
     def cb(self, msg):
         curr_rostime = rospy.get_rostime()
         curr = curr_rostime.to_sec()
         self._event.set()
 
-        self._lock.acquire()
+        self._filter_lock.acquire()
+
         if self._msg_t0 < 0 or self._msg_t0 > curr:
             self._msg_t0 = curr
             self._msg_tn = curr
@@ -103,7 +94,28 @@ class HzBase():
             self._filter.update(curr - self._msg_tn)
             self._msg_tn = curr
 
-        self._lock.release()
+        self.remaining_timeouts = self.max_timeouts
+
+        self._filter_lock.release()
+
+    def stop(self):
+        self._event.stop()
+
+
+class HzMergedBase():
+    def __init__(self, topic, config):
+        callerids = Config.get_param(config, 'callerid')
+        self.use_all_bases = True if not len(callerids) else False
+
+        self._states = []
+        for state_config in Config.get_param(config, 'states'):
+            try:
+                self._states.append(HzState(state_config))
+            except KeyError as e:
+                rospy.logerr(e)
+
+        self._resource_info = resource_info(type='hz', resource=str(topic + ' ' + str(callerids)))
+        self._observer_error = observer_error(type='hz', resource=str(topic + ' ' + str(callerids)))
 
     def _get_valied_states(self, value, deviation=[]):
         states = []
@@ -117,14 +129,35 @@ class HzBase():
             error_pub.publish(self._observer_error)
         return states
 
-    def get_resource_info(self):
-        mean, deviation, sample_size = self._filter.get_value()
-        if mean is None or sample_size < 2:
+    def get_resource_info(self, callerids):
+        if not len(callerids):
             self._resource_info.states = []
-        else:
-            print mean,  deviation, sample_size
-            self._resource_info.states = self._get_valied_states(mean, deviation)
+            self._resource_info.header = rospy.Header(stamp=rospy.Time.now())
+            return self._resource_info
 
+        is_first = True
+        mean = 0
+        deviation = []
+        sample_size = 0
+        for callerid in callerids.itervalues():
+            _mean, _deviation, _sample_size = callerid._filter.get_value()
+            print ' ', _mean,  _deviation, _sample_size
+
+            if _mean is None or _sample_size < 2:
+                continue
+
+            if is_first:
+                mean = _mean
+                deviation = _deviation
+                sample_size = _sample_size
+                is_first = False
+            else:
+                mean = (mean * _mean) / (mean + _mean)
+                deviation = [sum(x) for x in zip(deviation, _deviation)]
+                sample_size += _sample_size
+
+        print '>', mean, deviation, sample_size
+        self._resource_info.states = self._get_valied_states(mean, deviation)
         self._resource_info.header = rospy.Header(stamp=rospy.Time.now())
         return self._resource_info
 
@@ -134,71 +167,86 @@ class HzSubs():
         self.topic = Config.get_param(config, 'name')
 
         self.bases = dict()
+        self.bases_lock = Lock()
 
         self.callerids_config = Config.get_param(config, 'callerids')
+        self.merged_bases = []
+        self.config_for_unknown = None
 
-        # for callerid_config in callerids:
-        #     try:
-        #         callerid = Config.get_param(callerid_config, 'callerid')
-        #         callerid_unknown = True if callerid == 'unknown' else False
-        #         self.bases[callerid] = HzBase(self.topic, callerid_config)
-        #     except KeyError as e:
-        #         rospy.logerr(e)
+        for callerid_config in self.callerids_config:
+            try:
+                callerid_list = Config.get_param(callerid_config, 'callerid')
+                self.merged_bases.append(HzMergedBase(self.topic, callerid_config))
+                if not len(callerid_list):
+                    self.config_for_unknown = callerid_config
+            except KeyError as e:
+                rospy.logerr(e)
 
-        # if not len(self.bases):
-        #     raise StandardError("No Subscriber possible for topic '" + self.topic + "' maybe because of errors in config!")
-        # if callerid_unknown:
-        #     self.sub = rospy.Subscriber(self.topic, rospy.AnyMsg, self.pre_cb, queue_size=1)
-        # else:
-        #     self.sub = rospy.Subscriber(self.topic, rospy.AnyMsg, self.cb, queue_size=1)
         self.sub = rospy.Subscriber(self.topic, rospy.AnyMsg, self.cb, queue_size=1)
 
     def add_callerid(self, callerid):
         print 'new callerid schould be added'
 
-        callerid_unknown = False
-
         for callerid_config in self.callerids_config:
             callerid_config_name = callerid_config['callerid']
-            print callerid_config_name, callerid
-            # if callerid_config_name == 'unknown':
-            #     callerid_unknown = True
 
-            if callerid_config_name == callerid:
+            if callerid in callerid_config_name:
                 print "config for callerid found"
-                new_base = HzBase(self.topic, callerid_config)
+                new_base = HzBase(self.topic, callerid, callerid_config)
                 self.bases[callerid] = new_base
                 return new_base
 
-        # if callerid_unknown:
-        #     print "a 'unknown' is in list"
-        #     return None
+            if not len(callerid_config_name):
+                print "config for unknown callerid found"
+                new_base = HzBase(self.topic, callerid, callerid_config)
+                self.bases[callerid] = new_base
+                return new_base
 
         print 'nothing found'
-        new_base = HzBase(self.topic, None)
-        self.bases[callerid] = new_base
-        return new_base
-
-    # def pre_cb(self, msg):
-    #     self.bases['unknown'].cb(msg)
-    #     self.cb(msg)
+        self.bases[callerid] = None
+        return None
 
     def cb(self, msg):
         current_callerid = msg._connection_header['callerid']
-        print 'current callerid: ' + str(current_callerid)
-
-        base = self.bases.get(msg._connection_header['callerid'])
+        self.bases_lock.acquire()
+        if current_callerid not in self.bases:
+            base = self.add_callerid(current_callerid)
+        else:
+            base = self.bases.get(current_callerid)
 
         if base:
-            print 'callerid is in list'
             base.cb(msg)
-        else:
-            print 'callerid is NOT in list'
-            new_base = self.add_callerid(current_callerid)
-            new_base.cb(msg)
+        self.bases_lock.release()
 
     def get_resource_infos(self):
-        return [x.get_resource_info() for x in self.bases.itervalues() if not x.is_dummy_base]
+        resource_infos = []
+
+        for merge in self.merged_bases:
+
+            self.bases_lock.acquire()
+            if merge.use_all_bases:
+                callerids = dict(self.bases)
+            else:
+                callerids = dict((k, self.bases[k]) for k in merge.callerids if k in self.bases)
+            self.bases_lock.release()
+
+            resource_infos += [merge.get_resource_info(callerids)]
+            # print resource_infos
+
+        return resource_infos
+
+    def cleanup_dead_callerids(self):
+        self.bases_lock.acquire()
+
+        callerids = list(self.bases.iterkeys())
+
+        for callerid in callerids:
+            base = self.bases[callerid]
+            if base.remaining_timeouts is not None and base.remaining_timeouts < 1:
+                self.bases[callerid].stop()
+                del self.bases[callerid]
+
+        self.bases_lock.release()
 
 
 class Hz(PluginBase, PluginThread):
@@ -212,9 +260,6 @@ class Hz(PluginBase, PluginThread):
         error_pub = self.error_pub
 
     def run(self):
-        if not self.subs:
-            rospy.logwarn('No topics to work with!! Plugin will stop now.')
-            return
 
         rate = rospy.Rate(1)
 
@@ -222,6 +267,8 @@ class Hz(PluginBase, PluginThread):
             resource_data = []
             for sub in self.subs:
                 resource_data += sub.get_resource_infos()
+                sub.cleanup_dead_callerids()
+
 
             msg = observer_info(resource_infos=resource_data)
             self.info_pub.publish(msg)
