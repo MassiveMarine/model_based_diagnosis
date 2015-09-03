0557fdda9d836d7ef336f9678bea47f8f80de069
Stefan Loigge <stefan.loigge@student.tugraz.at>
2015-09-03 17:28:49 +0200

    removed deprecated methodes

--- tug_utils/tug_python_utils/scripts/tug_python_utils.py
+++ tug_utils/tug_python_utils/scripts/tug_python_utils.py
@@ -4,12 +4,11 @@
 class YamlHelper():
     @staticmethod
     def get_param(config, name):
-        if config.has_key(name):
+        if name in config:
             return config[name]
         print "ERROR"
         raise KeyError("'" + str(name) + "' not found in config!")
 
-
     @staticmethod
     def has_key(config, name):
-        return config.has_key(name)
+        return name in config
