860c51d834a2dc70e39a7a8cb163f9187b8a4da9
Clemens Muehlbacher <cmuehlba@ist.tugraz.at>
2015-09-15 14:59:36 +0200

    implement tf velocity conversion

--- tug_observers/tug_observer_plugins/tug_velocity_observer/include/tug_velocity_observer/VelocityConverterTf.h
+++ tug_observers/tug_observer_plugins/tug_velocity_observer/include/tug_velocity_observer/VelocityConverterTf.h
@@ -7,11 +7,15 @@
 
 #include <tug_velocity_observer/VelocityConverterPoseStamped.h>
 #include <nav_msgs/Odometry.h>
+#include <tf/transform_listener.h>
 
 class VelocityConverterTf : public VelocityConverterPoseStamped
 {
     ros::Rate tf_update_rate_;
     boost::thread background_thread_;
+    std::string base_frame_;
+    std::string target_frame_;
+    tf::TransformListener tf_listener_;
 public:
     VelocityConverterTf(XmlRpc::XmlRpcValue params, boost::function<void (MovementReading)> call_back, tug_observers::ObserverPluginBase* plugin_base);
     void run();
