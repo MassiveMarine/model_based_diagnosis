1bc43d72cb449967d2d4ff94673b2f6fcec89c46
Clemens Muehlbacher <cmuehlba@ist.tugraz.at>
2015-09-17 12:16:36 +0200

    add fixed std for student t test

--- tug_observers/tug_observer_plugins/tug_observer_plugin_utils/include/tug_observer_plugin_utils/hypothesis_check/singe_value_hypothesis_check/StudentsTSingleHypothesis.h
+++ tug_observers/tug_observer_plugins/tug_observer_plugin_utils/include/tug_observer_plugin_utils/hypothesis_check/singe_value_hypothesis_check/StudentsTSingleHypothesis.h
@@ -20,6 +20,8 @@ class StudentsTSingleHypothesis : public SingleValueHypothesisCheck<T>
 {
     T true_mean_;
     double significance_level_;
+    bool has_fixed_deviation_;
+    T std_deviation_;
 
     T absolut(T value)
     {
@@ -32,22 +34,40 @@ class StudentsTSingleHypothesis : public SingleValueHypothesisCheck<T>
 public:
     StudentsTSingleHypothesis(T true_mean, XmlRpc::XmlRpcValue params) : true_mean_(true_mean)
     {
-      significance_level_ = ProcessYaml::getValue<T>("significance_level", params);
+      significance_level_ = ProcessYaml::getValue<double>("significance_level", params);
+      if(ProcessYaml::hasValue("std_deviation", params))
+      {
+        has_fixed_deviation_ = true;
+        std_deviation_ = ProcessYaml::getValue<T>("std_deviation", params);
+      }
     }
 
     StudentsTSingleHypothesis(XmlRpc::XmlRpcValue params)
     {
       true_mean_ = ProcessYaml::getValue<T>("true_mean", params);
-      significance_level_ = ProcessYaml::getValue<T>("significance_level", params);
+      significance_level_ = ProcessYaml::getValue<double>("significance_level", params);
+      if(ProcessYaml::hasValue("std_deviation", params))
+      {
+        has_fixed_deviation_ = true;
+        std_deviation_ = ProcessYaml::getValue<T>("std_deviation", params);
+      }
     }
 
     virtual bool checkHypothesis(const FilteState<T>& state)
     {
-      if(!state.has_deviation || (state.deviation.size() != 1))
-        throw std::invalid_argument("student t test needs one deviation as parameter");
+      T std_deviation;
+      if(has_fixed_deviation_)
+        std_deviation = std_deviation_;
+      else
+      {
+        if(!state.has_deviation || (state.deviation.size() != 1))
+          throw std::invalid_argument("student t test needs one deviation as parameter");
+
+        std_deviation = state.deviation[0];
+      }
 
       T mean_difference = state.value - true_mean_;
-      T t_statistic = mean_difference * std::sqrt(static_cast<T>(state.sample_size)) / state.deviation[0];
+      T t_statistic = mean_difference * std::sqrt(static_cast<T>(state.sample_size)) / std_deviation;
 
       size_t freedoms = state.sample_size - 1;
       boost::math::students_t distribution(freedoms);
