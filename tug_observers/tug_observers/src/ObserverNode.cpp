3ec43fa18fa1e959a12ca88e43e656ff17aee640
Clemens Muehlbacher <cmuehlba@ist.tugraz.at>
2015-11-17 10:44:26 +0100

    make remaining package lint conform

--- tug_observers/tug_observers/src/ObserverNode.cpp
+++ tug_observers/tug_observers/src/ObserverNode.cpp
@@ -1,12 +1,26 @@
-//
-// Created by clemens on 03.08.15.
-//
+/*
+This file is part of the software provided by the tug ais groupe
+Copyright (c) 2015, Clemens Muehlbacher, Stefan Loige
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #include <tug_observers/ObserverNode.h>
+#include <string>
 
 namespace tug_observers
 {
-    ObserverNode::ObserverNode(ros::NodeHandle nh) : plugin_manager_("tug_observers", "tug_observers::ObserverPluginBase"), nh_(nh)
+    ObserverNode::ObserverNode(ros::NodeHandle nh) : plugin_manager_("tug_observers",
+                                                                     "tug_observers::ObserverPluginBase"), nh_(nh)
     { }
 
     void ObserverNode::initPlugins()
@@ -22,7 +36,7 @@ namespace tug_observers
 
       for (int i = 0; i < params.size(); ++i)
       {
-        XmlRpc::XmlRpcValue & param = params[i];
+        XmlRpc::XmlRpcValue &param = params[i];
 
         if (!param.hasMember("type"))
           throw std::runtime_error("/" + static_cast<std::string>(params[i]) + " has no 'type' parameter");
@@ -38,16 +52,16 @@ namespace tug_observers
       ROS_DEBUG("[ObserverNode::startPlugins] 1");
       Observers observers = plugin_manager_.getPluginList();
       ROS_DEBUG("[ObserverNode::startPlugins] 2");
-      for(Observers::iterator it = observers.begin(); it != observers.end(); ++it)
+      for (Observers::iterator it = observers.begin(); it != observers.end(); ++it)
       {
         ROS_DEBUG("[ObserverNode::startPlugins] 2.1");
         it->instance->startPlugin();
       }
       ROS_DEBUG("[ObserverNode::startPlugins] 2.2");
     }
-}
+}  // namespace tug_observers
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
   try
   {
@@ -61,7 +75,7 @@ int main(int argc, char** argv)
     ROS_INFO("plugins up and running");
     ros::spin();
   }
-  catch (std::exception & ex)
+  catch (std::exception &ex)
   {
     std::cerr << "Uncaught exception in main: " << ex.what() << std::endl;
     return -1;
@@ -71,7 +85,6 @@ int main(int argc, char** argv)
     std::cerr << "Uncaught exception in main" << std::endl;
     throw;
     return -1;
-
   }
-  return -1;
+  return 1;
 }
