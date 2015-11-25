cef45074aaad284033df2190769bf66d35abbc25
Stefan Loigge <stefan.loigge@student.tugraz.at>
2015-11-25 18:25:53 +0100

    activated observer got support for error propagation + unittests updated

--- tug_diagnosis/tug_diagnosis/scripts/pymbd/benchmark/tug_description_parser/observers/activated_observer.py
+++ tug_diagnosis/tug_diagnosis/scripts/pymbd/benchmark/tug_description_parser/observers/activated_observer.py
@@ -9,71 +9,42 @@ class ActivatedObserver(BaseObserver):
     Represents the fault injection logic used to enable/disable a gate's function.
     The implication ab_predicate -> gate_function
     """
-    def __init__(self, ab_node, topic):
+    def __init__(self, ab_node, observation):
         super(ActivatedObserver, self).__init__()
         checkInputData.str_data_valid(ab_node)
-        checkInputData.str_data_valid(topic)
+        checkInputData.str_data_valid(observation)
 
         self.ab_node = ab_node
-        self.topic = topic
+        self.observation = observation
 
     def __repr__(self):
-        return "activated: %s, %s" % (self.ab_node, self.topic)
+        return "activated: %s, %s" % (self.ab_node, self.observation)
 
     def to_clause(self):
-        return [clause(self.ab_node + " " + self.topic)]
+        return [clause(self.ab_node + " " + self.observation)]
 
     @staticmethod
-    def generate_model_parameter(config, topics_published_from_nodes, topics_subscribed_from_nodes={}):
+    def generate_model_parameter(config, topics_published_from_nodes, topics_subscribed_from_nodes,
+                                 nodes_publish_topics, nodes_subscribe_topics):
         checkInputData.dict_data_valid(config, False)
-        topics = config['topics']
+        nodes = config['nodes']
 
-        checkInputData.list_data_valid(topics)
-
-        checkInputData.dict_data_valid(topics_published_from_nodes, False)
+        checkInputData.list_data_valid(nodes)
 
         vars = {}
         rules = []
-        nodes = []
 
-        for topic in topics:
-            checkInputData.list_data_valid(topics_published_from_nodes[topic])
+        for node in nodes:
+            observation = "activated_obs_" + node
+            vars[observation] = Variable(observation, Variable.BOOLEAN, None)
+            rules.append(ActivatedObserver(ab_pred(str(node)), observation))
 
-            for callerid in topics_published_from_nodes[topic]:
-                observation = "activated_obs_" + topic + "_" + callerid
-                vars[observation] = Variable(observation, Variable.BOOLEAN, None)
-                rules.append(ActivatedObserver(ab_pred(str(callerid)), observation))
-
-            new_vars, new_rules, new_nodes = CalleridsObserver.generate_model_parameter("activated", topic, topics_published_from_nodes[topic])
-            vars.update(new_vars)
-            rules += new_rules
-            nodes += new_nodes
-
-        return vars, rules, nodes
+        return vars, rules, []
 
     @staticmethod
     def decrypt_resource_info(resource_info):
-
-        if not resource_info:
-            raise ValueError
-        if not isinstance(resource_info, str):
-            raise TypeError
-
-        [topic_name, callerids_str] = resource_info.split(' ', 1)
-
-        checkInputData.str_data_valid(topic_name)
-
-        if len(callerids_str) <= 2:
-            return ['activated_obs_' + str(topic_name) + "_all"]
-
-        callerids = [x.strip() for x in callerids_str[1:-1].split(',')]
-
-        infos = []
-        for callerid in callerids:
-            checkInputData.str_data_valid(callerid)
-            infos.append('activated_obs_' + str(topic_name) + "_" + str(callerid))
-
-        return infos
+        checkInputData.str_data_valid(resource_info, forbidden_chars=[' '])
+        return ['activated_obs_' + resource_info]
 
 
 picosat.SENTENCE_INTERPRETERS[ActivatedObserver] = lambda engine, pred, unused: pred.to_clause()
@@ -87,129 +58,116 @@ class TestActivatedObserver(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_activated_observer(self):
-        with self.assertRaises(ValueError):
-            ActivatedObserver(ab_pred(""), "/topic")
-        with self.assertRaises(ValueError):
-            ActivatedObserver(ab_pred("/"), "/topic")
-        with self.assertRaises(ValueError):
-            ActivatedObserver("", "/topic")
-        with self.assertRaises(ValueError):
-            ActivatedObserver("/", "/topic")
-        with self.assertRaises(ValueError):
-            ActivatedObserver(ab_pred("name"), "")
-        with self.assertRaises(ValueError):
-            ActivatedObserver(ab_pred(""), "")
-        with self.assertRaises(ValueError):
-            ActivatedObserver("", "")
-        with self.assertRaises(TypeError):
-            ActivatedObserver(1, "/topic")
-        with self.assertRaises(TypeError):
-            ActivatedObserver(ab_pred("name"), 1)
-        with self.assertRaises(ValueError):
-            ActivatedObserver(ab_pred("name"), "/")
+    def test_activated_observer1(self):
+        ab_node = ab_pred("/node1")
+        observation = "/node1"
+
+        ab_node_tests = [
+            (ValueError, ab_pred("")),
+            (ValueError, ab_pred("/")),
+            (ValueError, ""),
+            (ValueError, "/"),
+            (TypeError, 1),
+        ]
+
+        for (error, ab_node) in ab_node_tests:
+            with self.assertRaises(error):
+                print "'" + str(error.__name__) + "' should be raised by '" + str(ab_node) + "'",
+                ActivatedObserver(ab_node, observation)
+            print "... DONE"
+
+    def test_activated_observer2(self):
+        ab_node = ab_pred("/node1")
+        observation = "/topic"
+
+        observation_tests = [
+            (ValueError, ""),
+            (ValueError, "/"),
+            (TypeError, 1),
+        ]
+
+        for (error, observation) in observation_tests:
+            with self.assertRaises(error):
+                print "'" + str(error.__name__) + "' should be raised by '" + str(observation) + "'",
+                ActivatedObserver(ab_node, observation)
+            print "... DONE"
 
     def test_clause(self):
-        observer = ActivatedObserver(ab_pred("name"), "/topic")
+        observer = ActivatedObserver(ab_pred("name"), "/name")
         new_clause = observer.to_clause()[0]
         self.assertEqual(len(new_clause.literals), 2, "Number of 'literals' in clause does not match!")
         self.assertEqual(str(new_clause.literals[0]), ab_pred("name"), "First literal in clause does not match! ")
-        self.assertEqual(str(new_clause.literals[1]), "/topic", "Second literal in clause does not match!")
+        self.assertEqual(str(new_clause.literals[1]), "/name", "Second literal in clause does not match!")
 
-    def test_generate_model_parameter(self):
-        config = {'topics': ['/topic'], 'type': 'activated'}
-        topics_from_nodes = {'/topic': ['/node1', '/node2']}
-        vars, rules, nodes = ActivatedObserver.generate_model_parameter(config, topics_from_nodes)
+    def test_generate_model_parameter1(self):
+        config = {'nodes': ['/node1', '/node2', '/node3'], 'type': 'activated'}
+        topics_published_from_nodes = {'/topic': ['/node1', '/node2']}
+        topics_subscribed_from_nodes = {}
+        nodes_publish_topics = {'/node1': ['/topic'], '/node2': ['/topic']}
+        nodes_subscribe_topics = {}
+        vars, rules, nodes = ActivatedObserver.generate_model_parameter(config, topics_published_from_nodes, topics_subscribed_from_nodes, nodes_publish_topics, nodes_subscribe_topics)
 
-        vars_req = {'activated_obs_/topic_all': Variable('activated_obs_/topic_all', 1, None),
-                    'activated_obs_/topic_/node1': Variable('activated_obs_/topic_/node1', 1, None),
-                    'activated_obs_/topic_/node2': Variable('activated_obs_/topic_/node2', 1, None)}
+        vars_req = {'activated_obs_/node1': Variable('activated_obs_/node1', 1, None),
+                    'activated_obs_/node2': Variable('activated_obs_/node2', 1, None),
+                    'activated_obs_/node3': Variable('activated_obs_/node3', 1, None)}
 
-        self.assertEqual(len(vars), len(vars_req), "activated added wrong number of variables!")
+        self.assertEqual(len(vars), len(vars_req), "Activated added wrong number of variables!")
         for i, obj in vars.items():
             self.assertTrue(vars_req.has_key(i), "Key '" + str(i) + "' not in variables-required list!")
             self.assertEqual(str(vars_req[i]), str(obj), "Variable '" + str(i) + "' not generated with right parameters!")
 
-        rules_req = [ActivatedObserver(ab_pred('/node1'), 'activated_obs_/topic_/node1'),
-                     ActivatedObserver(ab_pred('/node2'), 'activated_obs_/topic_/node2'),
-                     CalleridsObserver('activated_obs_/topic_all', ['activated_obs_/topic_/node1', 'activated_obs_/topic_/node2'])]
+        rules_req = [ActivatedObserver(ab_pred('/node1'), 'activated_obs_/node1'),
+                     ActivatedObserver(ab_pred('/node2'), 'activated_obs_/node2'),
+                     ActivatedObserver(ab_pred('/node3'), 'activated_obs_/node3')]
 
         rules_req_str = [str(x) for x in rules_req]
         self.assertTrue(not any([x for x in rules if str(x) not in rules_req_str]), "Rules does not match!")
-        self.assertEqual(len(rules), len(rules_req), "activated added wrong number of rules!")
-        self.assertEqual(len(nodes), 0, "activated should not add nodes!")
+        self.assertEqual(len(rules), len(rules_req), "Activated added wrong number of rules!")
+        self.assertEqual(len(nodes), 0, "Activated should not add nodes!")
 
-        # test different arguments for the config-parameter which all should raise exeptions
-        topics_from_nodes = {'/topic': ['/node1', '/node2']}
-        with self.assertRaises(KeyError):
-            ActivatedObserver.generate_model_parameter({'topics_wrong_name': ['/topic'], 'type': 'activated'}, topics_from_nodes)
-        with self.assertRaises(KeyError):
-            ActivatedObserver.generate_model_parameter({'type': 'activated'}, topics_from_nodes)
-        with self.assertRaises(KeyError):
-            ActivatedObserver.generate_model_parameter({}, topics_from_nodes)
-        with self.assertRaises(TypeError):
-            ActivatedObserver.generate_model_parameter("not_a_dict", topics_from_nodes)
-        with self.assertRaises(TypeError):
-            ActivatedObserver.generate_model_parameter(1, topics_from_nodes)
-        with self.assertRaises(ValueError):
-            ActivatedObserver.generate_model_parameter({'topics': [], 'type': 'activated'}, topics_from_nodes)
-        with self.assertRaises(ValueError):
-            ActivatedObserver.generate_model_parameter({'topics': [''], 'type': 'activated'}, topics_from_nodes)
-        with self.assertRaises(TypeError):
-            ActivatedObserver.generate_model_parameter({'topics': [1], 'type': 'activated'}, topics_from_nodes)
-        with self.assertRaises(TypeError):
-            ActivatedObserver.generate_model_parameter({'topics': "no_list", 'type': 'activated'}, topics_from_nodes)
-        with self.assertRaises(TypeError):
-            ActivatedObserver.generate_model_parameter({'topics': 1, 'type': 'activated'}, topics_from_nodes)
+    def test_generate_model_parameter_errors_1(self):
+        config = {'nodes': ['node1', 'node2', 'node3'], 'type': 'activated'}
+        topics_published_from_nodes = {'/topic3': ['node3'], '/topic2': ['node2'], '/topic1': ['node1']}
+        topics_subscribed_from_nodes = {'node3': ['/topic2'], 'node2': ['/topic1']}
+        nodes_publish_topics = {'node1': ['/topic1'], 'node3': ['/topic3'], 'node2': ['/topic2']}
+        nodes_subscribe_topics = {'node3': ['/topic2'], 'node2': ['/topic1']}
 
-        # test different arguments for the topics_from_nodes-parameter which all should raise exeptions
-        config = {'topics': ['/topic'], 'type': 'activated'}
-        with self.assertRaises(KeyError):
-            ActivatedObserver.generate_model_parameter(config, {'/topic_wrong_name': ['/node1', '/node2']})
-        with self.assertRaises(ValueError):
-            ActivatedObserver.generate_model_parameter(config, {'/topic': []})
-        with self.assertRaises(KeyError):
-            ActivatedObserver.generate_model_parameter(config, {})
-        with self.assertRaises(TypeError):
-            ActivatedObserver.generate_model_parameter(config, "no_dict")
-        with self.assertRaises(TypeError):
-            ActivatedObserver.generate_model_parameter(config, 1)
-        with self.assertRaises(ValueError):
-            ActivatedObserver.generate_model_parameter(config, {'/topic': ['/', '/node2']})
-        with self.assertRaises(ValueError):
-            ActivatedObserver.generate_model_parameter(config, {'/topic': ['', '/node2']})
-        with self.assertRaises(TypeError):
-            ActivatedObserver.generate_model_parameter(config, {'/topic': [1, '/node2']})
-        with self.assertRaises(ValueError):
-            ActivatedObserver.generate_model_parameter(config, {'/topic': ['/node1', '/']})
-        with self.assertRaises(ValueError):
-            ActivatedObserver.generate_model_parameter(config, {'/topic': ['/node1', '']})
-        with self.assertRaises(TypeError):
-            ActivatedObserver.generate_model_parameter(config, {'/topic': ['/node1', 1]})
+        config_tests = [(KeyError, {'nodes_wrong_name': ['node1'], 'type': 'activated'}),
+                        (KeyError, {'type': 'activated'}),
+                        (KeyError, {}),
+                        (TypeError, "not_a_dict"),
+                        (TypeError, 1),
+                        (ValueError, {'nodes': [], 'type': 'activated'}),
+                        (ValueError, {'nodes': [''], 'type': 'activated'}),
+                        (TypeError, {'nodes': [1], 'type': 'activated'}),
+                        (TypeError, {'nodes': "no_list", 'type': 'activated'}),
+                        (TypeError, {'nodes': 1, 'type': 'activated'})
+                        ]
+
+        for (error, config) in config_tests:
+            with self.assertRaises(error):
+                print "'" + str(error.__name__) + "' should be raised by '" + str(config) + "'",
+
+                ActivatedObserver.generate_model_parameter(config,
+                                                           topics_published_from_nodes, topics_subscribed_from_nodes,
+                                                           nodes_publish_topics, nodes_subscribe_topics)
+            print "... DONE"
 
     def test_decrypt_resource_info(self):
-        self.assertEqual(ActivatedObserver.decrypt_resource_info("/topic_name [node1, node2]"), ['activated_obs_/topic_name_node1', 'activated_obs_/topic_name_node2'], "Topic name decryption not correct!")
-        self.assertEqual(ActivatedObserver.decrypt_resource_info("/topic_name [node1, node2, node3, node4, node5]"), ['activated_obs_/topic_name_node1', 'activated_obs_/topic_name_node2', 'activated_obs_/topic_name_node3',
-                                                                                                               'activated_obs_/topic_name_node4', 'activated_obs_/topic_name_node5'], "Topic name decryption not correct!")
-        self.assertEqual(ActivatedObserver.decrypt_resource_info("/topic_name [node1]"), ['activated_obs_/topic_name_node1'], "Topic name decryption not correct!")
-        self.assertEqual(ActivatedObserver.decrypt_resource_info("/topic_name {}"), ['activated_obs_/topic_name_all'], "Topic name decryption not correct!")
+        self.assertEqual(ActivatedObserver.decrypt_resource_info("/node1"), ['activated_obs_/node1'], "Topic name decryption not correct!")
 
-        with self.assertRaises(ValueError):
-            ActivatedObserver.decrypt_resource_info("/ [node1, node2]")
-        with self.assertRaises(ValueError):
-            ActivatedObserver.decrypt_resource_info("/ [node1, /]")
-        with self.assertRaises(ValueError):
-            ActivatedObserver.decrypt_resource_info("/topic_name")
-        with self.assertRaises(ValueError):
-            ActivatedObserver.decrypt_resource_info("/topic_name [/]")
-        with self.assertRaises(ValueError):
-            ActivatedObserver.decrypt_resource_info("/topic_name [/node1, ]")
-        with self.assertRaises(ValueError):
-            ActivatedObserver.decrypt_resource_info("/topic_name [/node1, /]")
-        with self.assertRaises(ValueError):
-            ActivatedObserver.decrypt_resource_info("/")
-        with self.assertRaises(ValueError):
-            ActivatedObserver.decrypt_resource_info("")
-        with self.assertRaises(TypeError):
-            ActivatedObserver.decrypt_resource_info(1)
+        resource_info_tests = [
+            (ValueError, "/"),
+            (ValueError, ""),
+            (ValueError, "/node_name "),
+            (ValueError, "/node_name []"),
+            (ValueError, "/node_name some_wrong_text"),
+            (TypeError, 1),
+            ]
+
+        for (error, resource_info) in resource_info_tests:
+            with self.assertRaises(error):
+                print "'" + str(error.__name__) + "' should be raised by '" + str(resource_info) + "'",
+                ActivatedObserver.decrypt_resource_info(resource_info)
+            print "... DONE"
 
