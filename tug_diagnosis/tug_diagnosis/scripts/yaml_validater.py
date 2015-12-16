3a75d60442cd0054a26dce3c1a277ff4429378ad
Stefan Loigge <stefan.loigge@student.tugraz.at>
2015-12-16 14:18:21 +0100

    further implementation for yaml validater + launch file

--- tug_diagnosis/tug_diagnosis/scripts/yaml_validater.py
+++ tug_diagnosis/tug_diagnosis/scripts/yaml_validater.py
@@ -31,12 +31,12 @@ if __name__ == "__main__":
 
     published_topics = [topic for topic in topics_from_nodes if topic in topics_published_from_nodes.keys()]
     not_published_topics = [topic for topic in topics_from_nodes if topic not in topics_published_from_nodes.keys()]
-    print 'Topics that are published / not published:   %3d / %3d' % (len(published_topics), len(not_published_topics))
+    rospy.logwarn('Topics that are published / not published:   %3d / %3d' % (len(published_topics), len(not_published_topics)))
     print not_published_topics
 
     subscribed_topics = [topic for topic in topics_from_nodes if topic in topics_subscribed_from_nodes.keys()]
     not_subscribed_topics = [topic for topic in topics_from_nodes if topic not in topics_subscribed_from_nodes.keys()]
-    print 'Topics that are subscribed / not subscribed: %3d / %3d' % (len(subscribed_topics), len(not_subscribed_topics))
+    rospy.logwarn('Topics that are subscribed / not subscribed: %3d / %3d' % (len(subscribed_topics), len(not_subscribed_topics)))
     # print not_subscribed_topics
 
     topics = set()
@@ -49,16 +49,20 @@ if __name__ == "__main__":
                     topics.update([item])
                 else:
                     topics.update(item)
-    print 'Topics that are observerd by diagnosis config: %3d' %(len(topics))
+    rospy.logwarn('Topics that are observerd by diagnosis config: %3d' %(len(topics)))
 
-    print 'Topics that are known: %3d' %(len(topics_from_nodes | topics))
+    rospy.logwarn('Topics that are known: %3d' %(len(topics_from_nodes | topics)))
 
     ghost_topics = [topic for topic in topics if topic not in topics_from_nodes]
-    print 'Topics observed but no node subscribes or publishs: %3d' % (len(ghost_topics))
+    rospy.logwarn('Topics observed but no node subscribes or publishs: %3d' % (len(ghost_topics)))
     print ghost_topics
 
+    publishless_topics = [topic for topic in topics if topic not in published_topics]
+    rospy.logwarn('Topics observed but without publishing node: %3d' % (len(publishless_topics)))
+    print publishless_topics
+
     useless_topics = [topic for topic in topics_from_nodes if topic not in topics]
-    print 'Topics subscribes or publishs by nodes but not observed: %3d' % (len(useless_topics))
+    rospy.logwarn('Topics subscribes or publishs by nodes but not observed: %3d' % (len(useless_topics)))
     print useless_topics
 
     all_topics = topics_from_nodes | topics
@@ -66,12 +70,6 @@ if __name__ == "__main__":
     for entry in all_topics:
         if any(x in entry for x in ['$', '#', '|', ' ', '-', 'AB', '/AB']):
             forbidden_topics.update([entry])
-    print 'Topics that are with forbidden char: %3d' %(len(forbidden_topics))
+    rospy.logwarn('Topics that contains forbidden characters: %3d' %(len(forbidden_topics)))
     print list(forbidden_topics)
-
-
-
-
-
-
 
